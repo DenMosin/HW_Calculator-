1caa2d0742028585ca7bd53488c773d8f55df682
Denis Mosin <denmsin@mail.ru>
2023-01-17 17:03:21 +0500

    change HW

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,17 +8,10 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> (y == 0) ? 0 : y;
 
     UnaryOperator<Integer> pow = x -> x * x;
-    UnaryOperator<Integer> abs = x -> {
-        if (x == 0) {
-            System.out.println("Делить на 0 нельзя");
-            System.exit(0);
-        }
-        return x;
-    };
-
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
 
     Predicate<Integer> isPositive = x -> x > 0;
 
